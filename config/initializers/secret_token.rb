4447c2ca4fbe0ac74e3f0e33831e47b832fb6818
Duncan Fermor <duncan.fermor@gmail.com>
2014-03-23 21:37:23 +0000

    initial rails setup with posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestBlog::Application.config.secret_key_base = '3a53313d9765b13fb638035b9785164ae934de247438c13444e465867411100dbf93c9694bcf07815ee7ccc0c04dff1f534bb814ee4050f458f4681925b2a018'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+TestBlog::Application.routes.draw do
+
+  resources :posts
+
+  root to: "welcome#index"
+end
